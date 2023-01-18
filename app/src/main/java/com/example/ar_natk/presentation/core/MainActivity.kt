6a954d430646f94579cc1f468b6bc75995fbcdc3
Konstantin <konstkonst55@mail.ru>
2023-01-18 23:04:52 +0700

    Исправлен SplashScreen

--- app/src/main/java/com/example/ar_natk/presentation/core/MainActivity.kt
+++ app/src/main/java/com/example/ar_natk/presentation/core/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ar_natk.presentation.core
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
@@ -8,7 +9,11 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.ar_natk.R
+import com.example.ar_natk.data.storage.FirstRunStorage
+import com.example.ar_natk.data.storage.UserDataStorage
 import com.example.ar_natk.databinding.ActivityMainBinding
+import com.example.ar_natk.presentation.auth.AuthActivity
+import com.example.ar_natk.presentation.onboard.OnboardActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,19 +25,37 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_AR_NATK)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setTheme(R.style.Theme_AR_NATK)
         setContentView(binding.root)
         setSupportActionBar(binding.tbMain)
 
-        navView = binding.navView
+        checkRun()
+    }
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_main) as NavHostFragment
-        navController = navHostFragment.navController
+    private fun checkRun() {
+        when (FirstRunStorage(this).isFirstRun) {
+            true -> {
+                startActivity(Intent(this, OnboardActivity::class.java))
+                finish()
+                FirstRunStorage(this).isFirstRun = false
+            }
+            false -> {
+                if (UserDataStorage(this).userName.isNullOrEmpty()) {
+                    startActivity(Intent(this, AuthActivity::class.java))
+                    finish()
+                } else {
+                    navView = binding.navView
 
-        setupAppBar()
+                    val navHostFragment =
+                        supportFragmentManager.findFragmentById(R.id.nav_host_main) as NavHostFragment
+                    navController = navHostFragment.navController
+
+                    setupAppBar()
+                }
+            }
+        }
     }
 
     private fun setupAppBar() {
